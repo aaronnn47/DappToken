b1ff42a3d16471ffc623df308b6a91f763d72577
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2018-10-10 16:26:13 -0600

    3. delegated transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -23,13 +23,26 @@ contract DappToken{
         address indexed _to,
         uint256 _value
     );
+    // approve event
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+    // transfer event
 
     mapping(address => uint256) public balanceOf;
+    
+    //allowance
+    mapping(address => mapping(address => uint256)) public allowance;
 
     function DappToken (uint256 _initialSupply) public {
+
         balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
+
         //allocate the initial supply
+        totalSupply = _initialSupply;
     }
     // transfer
     function transfer(address _to, uint256 _value) public returns (bool success){
@@ -45,6 +58,39 @@ contract DappToken{
 
     // return boolean
 
+        return true;
+    }
+
+    // Delegated Transfer
+    
+    // approve function
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        //allowance 
+        allowance[msg.sender][_spender] = _value;
+
+        //approve event
+        Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+        
+        // require _from has enough tokens
+        require(_value <= balanceOf[_from]);
+        // require allowance is big enough
+        require(_value <= allowance[_from][msg.sender]);
+        // change the balanceOf
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        // update the allowance
+        allowance[_from][msg.sender] -= _value;
+        //transfer event
+        Transfer(_from,_to,_value);
+
+        //return a boolean
         return true;
     }
 } 
